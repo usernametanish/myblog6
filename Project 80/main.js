d78a796a75a97cccf2028a9cf6ba6b6ccd02d929
usernametanish <76635990+usernametanish@users.noreply.github.com>
2021-03-24 15:45:53 -0500

    Add files via upload

---
+++ Project 80/main.js
@@ -0,0 +1,53 @@
+function get1(){
+    var inputs = [];
+    for(var i=1; i<6; i++)
+    inputs.push (document.getElementById("box_1_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_1").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_2_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_2").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_3_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_3").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_4_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_4").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_5_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_5").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_6_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_6").innerHTML = inputs.join (". ");
+    
+}
+
+
+
+
+
+
+
+
+
+function get2(){
+    var inputs2 = [];
+    for(var k=1; k<6; k++)
+    inputs.push (document.getElementById("box_7_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_7").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_8_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_8").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_9_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_9").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_10_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_10").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_11_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_11").innerHTML = inputs.join (". ");
+    inputs.push (document.getElementById("box_12_"+i).value);
+    inputs.join(". ");
+    document.getElementById("show_paragraph_12").innerHTML = inputs.join (". ");
